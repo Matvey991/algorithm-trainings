fcb93e048c1a933fb1eecc2e2cc5cd5f423a6083
Matvey991 <matveyfedotov61@gmail.com>
2024-06-11 01:17:58 +0400

    js2_task_3.4.

---
+++ js2_task_3.4.js
@@ -0,0 +1,8 @@
+// В программе задан массив array. Напишите функцию count(), которая считает количество 
+// элементов массива array и выводит в консоль сообщение, как в примере.
+let array = [true, 4, "word", "10n"]
+let res = array.length
+function count(array) {
+console.log(`Количество элементов в массиве: ${res}`)
+}
+count(array)
