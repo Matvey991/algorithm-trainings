79791f0729cfc2b7676e9a4a4e6415adcd67e218
Matvey991 <matveyfedotov61@gmail.com>
2024-06-15 15:11:45 +0400

    js2_task_4.1

---
+++ js2_task_4.1.js
@@ -0,0 +1,25 @@
+// В программе объявлены объекты pet_1 и pet_2. 
+// Напишите функцию getName() и присвойте ее объектам pet_1 и pet_2 в качестве метода. 
+// При вызове метода getName() он должен вернуть имя и возраст питомца через пробел. 
+// Решить задачу необходимо с использованием this.
+let pet_1 = { 
+    name: "Шарик", 
+    age: 10 
+};
+
+let pet_2 = { 
+    name: "Жучка", 
+    age: 5
+};
+pet_2.new_name = pet_2.name
+delete pet_2.name
+pet_2.new_age = pet_2.age
+delete pet_2.age
+let res = {...pet_1, ...pet_2}
+getName = getName.bind(res)
+function getName() {
+    let arr = `${this.name} ${this.age}; `
+    let arr1 = `${this.new_name} ${this.new_age}`
+    return arr + arr1
+}
+console.log(getName())
