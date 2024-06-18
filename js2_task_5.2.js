0bf0c3fa7e7ef0e6074874910b892e1ae2fec1bf
Matvey991 <matveyfedotov61@gmail.com>
2024-06-18 23:17:44 +0400

    js2_task_5.2

---
+++ js2_task_5.2.js
@@ -0,0 +1,18 @@
+// В программе задан объект employees, который содержит ряд свойств и методов. 
+// Сформируйте строку с именами всех свойств через запятую и пробел (, ) и выведите в консоль. 
+// Имена методов добавляться не должны.
+let employees = {
+    firstName: "Петя",
+    lastName: "Иванов",
+    ratePerDay: 2500,
+    workingDays: 5,
+    getSalary() { console.log(employees.ratePerDay * employees.workingDays )} 
+}
+let arr = []
+for (let i in employees) {
+let res = typeof employees[i]
+if (res === 'function') {
+delete employees[i]
+}
+    }
+console.log(Object.keys(employees).join(', '))
