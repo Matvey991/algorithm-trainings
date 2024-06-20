12e503f7f2a199e7e909a3dca1359e8c4cc6ab86
Matvey991 <matveyfedotov61@gmail.com>
2024-06-21 00:08:10 +0400

    task5

---
+++ task5.js
@@ -0,0 +1,10 @@
+// Напишите функцию, которая принимает целое число nи строку sв качестве параметров и возвращает строку, sповторяющуюся ровно nраз.
+// Примеры (ввод -> вывод)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+function repeatStr() {
+let n = 'iiii'
+let s = 'Hello'
+return s.repeat(n)
+}
+repeatStr()
