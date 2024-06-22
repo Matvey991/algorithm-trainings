08ba92b2b826768c32549135989613de689ad6b9
Matvey991 <matveyfedotov61@gmail.com>
2024-06-23 00:47:20 +0400

    js2_task_2.15

--- js2_task_2.15.js
+++ js2_task_2.15.js
@@ -1,15 +1,15 @@
-// В программе задана переменная values, которая хранит массив из строк.
-//  Определите математическую сумму всех элементов, которые при преобразовании в число не вернут значение NaN. 
-//  Результат выведите в консоль.
-let values = ["100", "30", "Не число", "20", "Тоже не число"]
-let sum = []
-for (let i = 0; i < values.length; i++) {
-if (!isNaN(values[i])) {
-sum.push(values[i]) 
-}
-}
-let transformation = sum.map(Number)
-let res = transformation.reduce(function (previousValue, currentItem) {
-   return previousValue + currentItem
-}, 0)
-console.log(res)
+ // В программе задана переменная values, которая хранит массив из строк.
+ //  Определите математическую сумму всех элементов, которые при преобразовании в число не вернут значение NaN. 
+ //  Результат выведите в консоль.
+ let values = ["100", "30", "Не число", "20", "Тоже не число"]
+ let sum = []
+ for (let i = 0; i < values.length; i++) {
+ if (!isNaN(values[i])) {
+ sum.push(values[i]) 
+ }
+ }
+ let transformation = sum.map(Number)
+ let res = transformation.reduce(function (previousValue, currentItem) {
+    return previousValue + currentItem
+ }, 0)
+ console.log(res)
