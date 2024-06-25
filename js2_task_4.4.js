b711837e9c45eefb6d378fcdfa3febdd5b54fefc
Matvey991 <matveyfedotov61@gmail.com>
2024-06-25 22:40:31 +0400

    js2_task_4.4

--- js2_task_4.4.js
+++ js2_task_4.4.js
@@ -1,20 +1,20 @@
-// В программе объявлен объект props, у которого задан набор свойств. 
-// Объявите функцию getValue(), которая выводит в консоль строку со всеми свойствами и их значениям.
-// Привяжите объект props в качестве контекста функции getValue() и присвойте получившуюся функцию переменной getValue.
-
-let props = { 
-    name: "Анатолий", 
-    age: 29,
-    sayHi: ()=>"привет"
-}
-function getValue() {
-    for (const key in this) {
-        if (typeof this[key] !== 'function') {
-            console.log(`${key}: ${this[key]}`);
-        }
-    }
-}
-
-const getValueBound = getValue.bind(props);
-getValueBound();
+ // В программе объявлен объект props, у которого задан набор свойств. 
+ // Объявите функцию getValue(), которая выводит в консоль строку со всеми свойствами и их значениям.
+ // Привяжите объект props в качестве контекста функции getValue() и присвойте получившуюся функцию переменной getValue.
+ 
+ let props = { 
+     name: "Анатолий", 
+     age: 29,
+     sayHi: ()=>"привет"
+ }
+ function getValue() {
+     for (const key in this) {
+         if (typeof this[key] !== 'function') {
+             console.log(`${key}: ${this[key]}`);
+          }
+     }
+ }
+ 
+ const getValueBound = getValue.bind(props);
+ getValueBound();
  
