03d3b625ecab60544583e845d61388e00a9201a5
Matvey991 <matveyfedotov61@gmail.com>
2024-06-25 22:38:41 +0400

    js2_task_3.5

--- js2_task_3.5.js
+++ js2_task_3.5.js
@@ -1,11 +1,11 @@
-// В программе задан двумерный массив employee. Удалите у данного массива значение, 
-// у которого первый элемент hireDate, 
-// и добавьте новое значение ["jobName", "IT PROG"] в конец массива. Результат выведите в консоль.
-let employee  = [
-    ["firstName", "Ivan"], 
-    ["lastName", "Ivanov"],
-    ["hireDate", "21.10.2015"]
-] 
-let res = employee.filter(e => e[0] !== 'hireDate') 
-res.push(["jobName", "IT PROG"])
-console.log(res) 
+ // В программе задан двумерный массив employee. Удалите у данного массива значение, 
+ // у которого первый элемент hireDate, 
+ // и добавьте новое значение ["jobName", "IT PROG"] в конец массива. Результат выведите в консоль.
+ let employee  = [
+     ["firstName", "Ivan"], 
+     ["lastName", "Ivanov"],
+     ["hireDate", "21.10.2015"]
+ ] 
+ let res = employee.filter(e => e[0] !== 'hireDate') 
+ res.push(["jobName", "IT PROG"])
+ console.log(res) 
