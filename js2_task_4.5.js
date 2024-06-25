451bb1b6ddac260b53b5e0e32468f2cb8be122b0
Matvey991 <matveyfedotov61@gmail.com>
2024-06-25 22:42:09 +0400

    js2_task_4.5

--- js2_task_4.5.js
+++ js2_task_4.5.js
@@ -1,12 +1,12 @@
-// В программе объявлен объект hero, свойства которого описывают информацию о герое. 
-// В объекте в том числе определен метод getPosition, который возвращает позицию героя в игре.
-// Определите, почему вызов getPosition работает некорректно. Исправьте эту ошибку.
-let hero = { 
-    nick: "FirstHero", 
-    position: "Лагерь",
-    getPosition: ()=>console.log("Позиция героя: " + `${this.position}`)
-}
-hero.getPosition = function() {
-    console.log("Позиция героя: " + this.position);
-}
-console.log(hero.getPosition()) 
+ // В программе объявлен объект hero, свойства которого описывают информацию о герое. 
+ // В объекте в том числе определен метод getPosition, который возвращает позицию героя в игре.
+ // Определите, почему вызов getPosition работает некорректно. Исправьте эту ошибку.
+ let hero = { 
+     nick: "FirstHero", 
+     position: "Лагерь",
+     getPosition: ()=>console.log("Позиция героя: " + `${this.position}`)
+ }
+ hero.getPosition = function() {
+     console.log("Позиция героя: " + this.position);
+ }
+ console.log(hero.getPosition()) 
