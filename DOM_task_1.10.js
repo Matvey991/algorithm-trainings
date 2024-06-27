30e026c248f602da1ca3a8c31717230cdf4a578b
Matvey991 <matveyfedotov61@gmail.com>
2024-06-28 00:36:26 +0400

    DOM_task_1.10

---
+++ DOM_task_1.10.js
@@ -0,0 +1,10 @@
+// Исправьте скрипт таким образом, чтобы в результате его работы в переменной result была сумма значений тегов с классом numeric.
+// Пример заданного HTML-документа:
+// <p class="numeric">62</p>
+// <p class="numeric">-12</p>
+// <p class="numeric">16</p>
+// Пример результата:
+// result = 66
+let result = 0;
+let elems = document.querySelectorAll(".numeric");
+elems.forEach(elem => result += +elem.innerText);
