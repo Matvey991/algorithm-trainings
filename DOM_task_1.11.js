ab70a5fe1cc16c8b7a8d741fa1eac452de31e7db
Matvey991 <matveyfedotov61@gmail.com>
2024-06-28 00:53:03 +0400

    DOM_task_1.11

---
+++ DOM_task_1.11.js
@@ -0,0 +1,13 @@
+// В HTML-документе задан тег <ul> c классом cars. Внутри этого тега есть несколько тегов <li>. Реализуйте скрипт, 
+// который считывает содержимое этих элементов <li>, формирует из них массив и сохраняет в переменной cars.
+// Пример заданного HTML-документа:
+// <ul id="cars">
+// <li>Tesla</li>
+// <li>Opel</li>
+// </ul>
+// Пример результата:
+// ["Tesla',"Opel"]
+let res = ''
+let arr = document.querySelector('.cars').innerText
+res += arr
+let cars = res.replace(/\n/g, ' ').split(' ')
