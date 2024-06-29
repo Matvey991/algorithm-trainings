409a80dadcdd73bd7f2b10cdc246fafc868c00f6
Matvey991 <matveyfedotov61@gmail.com>
2024-06-29 22:39:24 +0400

    DOM_task_2.4

---
+++ DOM_task_2.4.js
@@ -0,0 +1,13 @@
+// В HTML-документе есть несколько тегов <div>. Напишите скрипт, который всем тегам <div> добавляет класс adds.
+// Пример заданного HTML-документа:
+// <div><p>Реклама</p></div>
+// <div>Баннер</div>
+// Пример результата:
+// <div class="adds"><p>Реклама</p></div>
+// <div class="adds">Баннер</div>
+
+let res = document.querySelectorAll('div')
+for (let i = 0; i < res.length; i++) {
+    res[i].className = 'adds'
+    console.log(res[i])
+}
