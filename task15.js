6abc2b54df44e79fb6dc6f9c93e165635b79b4e7
Matvey991 <matveyfedotov61@gmail.com>
2024-06-30 23:17:14 +0400

    task15

--- task15.js
+++ task15.js
@@ -1,12 +1,12 @@
-// Вы получаете массив чисел, возвращаете сумму всех положительных чисел.
-// Пример [1,-4,7,12]=>1 + 7 + 12 = 20
-// Примечание. Если суммировать нечего, сумма по умолчанию равна 0.
-let arr = [1,-2,3,4,5] 
-function positiveSum(arr) {
-let filt = arr.filter(e => e > 0) 
-let res = filt.reduce(function(previousValue, currentItem) {
-   return previousValue + currentItem
-}, 0)
-return res 
-}
-positiveSum(arr) 
+   // Вы получаете массив чисел, возвращаете сумму всех положительных чисел.
+   // Пример [1,-4,7,12]=>1 + 7 + 12 = 20
+   // Примечание. Если суммировать нечего, сумма по умолчанию равна 0.
+   let arr = [1,-2,3,4,5] 
+   function positiveSum(arr) {
+   let filt = arr.filter(e => e > 0) 
+   let res = filt.reduce(function(previousValue, currentItem) {
+      return previousValue + currentItem
+   }, 0)
+   return res 
+   }
+   positiveSum(arr) 
