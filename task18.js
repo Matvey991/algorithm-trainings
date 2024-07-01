c2c2bcf57dd4f420686354e46b33765318d96277
Matvey991 <matveyfedotov61@gmail.com>
2024-07-02 02:39:21 +0400

    task18

---
+++ task18.js
@@ -0,0 +1,11 @@
+let a = ['t', 'e', 's', 't']
+let x = 'e'
+for (let i = 0; i < a.length; i++) {
+if (a.includes(x)){
+    console.log(true)
+    break
+} else {
+    console.log(false)
+    break
+}
+}
