ecedda9484b3c8dab3e46cc856125f88db9cbe6f
Matvey991 <matveyfedotov61@gmail.com>
2024-07-06 10:03:25 +0400

    DOM_task_2.12

---
+++ DOM_task_2.12.js
@@ -0,0 +1,18 @@
+// Напишите программу, которая считает количество <div>-, <p>- и <h2>-тегов и записывает его в объект tags, как в примере. Выведите сформированный объект в консоль.
+// Пример заданного HTML-документа:
+// <p>100</p>
+// <p>200</p>
+// <div>50</div>
+// <h2></h2>
+// <h2>40</h2>
+// Пример результата:
+// {"div": 1, "p": 2, "h2": 2}
+
+let arr = {}
+let div = document.querySelectorAll('div').length
+let p = document.querySelectorAll('p').length
+let h2 = document.querySelectorAll('h2').length
+arr.div = div
+arr.p = p
+arr.h2 = h2
+console.log(arr)
