49d88c46642e3fdcca40b1a6959cd4f0f7dc269e
Matvey991 <matveyfedotov61@gmail.com>
2024-07-10 23:12:34 +0400

    DOM_taak_3.1

---
+++ DOM_task_3.1.js
@@ -0,0 +1,9 @@
+// В HTML-документе задан тег <div> с идентификатором div_elem. Напишите скрипт, который добавит в стиль данного элемента ширину и высоту по 100 пикселей.
+function resizeDivAndLog() {
+    let div = document.getElementById('div_elem')
+        div.style.width = '100px'
+        div.style.height = '100px'
+    } 
+    
+    resizeDivAndLog()
+    
