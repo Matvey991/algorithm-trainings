9e7f57c7a218c1c19574cdc5c8673291408d8c71
Matvey991 <matveyfedotov61@gmail.com>
2024-07-11 23:41:46 +0400

    task29

---
+++ task29.js
@@ -0,0 +1,10 @@
+// Учитывая строку, необходимо вернуть строку, в которой каждый символ (с учетом регистра) повторяется один раз.
+let str = 'abc'
+function doubleChar(str) {
+    let res = ''
+    for (let i = 0; i < str.length; i++) {
+    res += str[i].repeat(2)
+    }
+    return res
+    }
+    
