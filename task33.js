0f0cf6e72fbe0e6ba1f80a1063709493a5fb754b
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-07-22 22:42:31 +0400

    task33

--- task33.js
+++ task33.js
@@ -1,16 +1,12 @@
-let arr = [1,2]
-function pra(arr) {
-let result = []
-if (arr.length <= 0) {
-return null
-} else {
-for (let i = 0; i < arr.length; i++) {
-if (arr[i] !== arr[i - 1] + 1) {
-result.push(arr[i])
-console.log(result[1])
-} else {
-return null
-}
-}
-}
-} pra(arr)
+// Имея двумерный (вложенный) список (массив, вектор, ..) размера m * n, ваша задача — найти сумму минимальных значений в каждой строке.
+
+// Например:
+
+// [ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+// , [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+// , [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+// ]
+// Таким образом, функция должна вернуть значение, 26поскольку сумма минимумов равна 1 + 5 + 20 = 26.
+
+let arr = [[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]
+console.log(arr.reduce((a, b) => a + Math.min(...c)),)
