8f8c8c997824a275e48d6e6b6e7b76c92ab5041f
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-08-10 21:23:05 +0400

    interviewTask_2

---
+++ interviewTask_2.js
@@ -0,0 +1,27 @@
+// Вернуть объект с ключами type, а значение - объект
+// вида {count: количество, weight: суммарный вес}
+
+const arr = [
+    { type: "banana", weight: 32 },
+    { type: "apple", weight: 24 },
+    { type: "kiwi", weight: 55 },
+    { type: "banana", weight: 44 },
+    { type: "orange", weight: 5 },
+  ];
+
+const groupByType = (arr) => {
+  const result = {}
+arr.forEach(item => {
+  const {type, weight} = item
+
+  if (result[type]) {
+    result[type].count++
+    result[type].weight += weight
+  } else {
+    result[type] = {count: 1, weight}
+  }
+});
+console.log(result);
+
+}
+groupByType(arr)
