3039f6af67d09969e8211c49388ad3ab0a05cbd4
Matvej Fedotov <matveyfedotov61@gmail.com>
2024-08-21 23:43:18 +0400

    interviewTask_3

---
+++ interviewTask_3.js
@@ -0,0 +1,16 @@
+// Дан список целых чисел и одно значение суммы, 
+// вернуть первые два значения (анализируйте слева направо) в порядке появления, которые в сумме образуют сумму.
+let arr = [11, 2, 7, 3]
+let num = 10
+function sumPars(arr, num) {
+    const seen = new Map()
+    for (let i = 0; i < arr.length; i++) {
+    const complemet = num - arr[i]        
+    if (seen.has(complemet)) {
+        return [complemet, arr[i]]
+    }
+    seen.set(arr[i], i)
+    }
+    return null
+}
+console.log(sumPars(arr, num));
